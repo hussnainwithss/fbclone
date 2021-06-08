b977c128189b969f727565d6766a1f244ce52ab0
Hussnain Ahmad <21100150@lums.edu.pk>
2021-06-08 15:09:52 +0500

    added boilerplate

---
+++ fbclone/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD',default=''),
+        'HOST': env('DATABASE_HOST',default='localhost'),
+        'PORT': env('DATABASE_PORT',default='5432'),
+    }
+}
