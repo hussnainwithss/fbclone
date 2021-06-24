e8b9aaba30d5e5d86c4dd795473a904acf6e00dc
Hussnain Ahmad <21100150@lums.edu.pk>
2021-06-24 14:33:04 +0500

    overrode first_name and last_name to make them mandatory

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,82 +6,15 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     """Form for handling CustomUser Object Creation
-    Makes sure first and last names are provided as
-    they are mandatory
-    Raises:
-        ValidationError: if either first or last name isnt provided
-        validationError is raised
     """
     class Meta:
         model = CustomUser
         fields = ('email',)
-
-    def clean_first_name(self):
-        """Called by form on validating form data
-        also provides the calling view with firstname
-
-        Raises:
-            ValidationError: if firstname iisnt provided
-
-        Returns:
-            str: FirstName from submitted form data
-        """
-        if self.cleaned_data["first_name"].strip() == '':
-            raise ValidationError("First name is required.")
-        return self.cleaned_data["first_name"]
-
-    def clean_last_name(self):
-        """
-        Called by form on validating form data
-        also provides the calling view with lastname
-
-        Raises:
-            ValidationError: if lastname iisnt provided
-
-        Returns:
-            str: lastname from submitted form data
-        """
-        if self.cleaned_data["last_name"].strip() == '':
-            raise ValidationError("Last name is required.")
-        return self.cleaned_data["last_name"]
 
 
 class CustomUserChangeForm(UserChangeForm):
     """Form for handling CustomUser Object Update
-    Makes sure first and last names are provided as
-    they are mandatory
-    Raises:
-        ValidationError: if either first or last name isnt provided
-        validationError is raised
     """
     class Meta:
         model = CustomUser
         fields = ('email',)
-
-    def clean_first_name(self):
-        """Called by form on validating form data
-        also provides the calling view with firstname
-
-        Raises:
-            ValidationError: if firstname iisnt provided
-
-        Returns:
-            str: FirstName from submitted form data
-        """
-        if self.cleaned_data["first_name"].strip() == '':
-            raise ValidationError("First name is required.")
-        return self.cleaned_data["first_name"]
-
-    def clean_last_name(self):
-        """Called by form on validating form data
-        also provides the calling view with lastname
-
-        Raises:
-            ValidationError: if lastname iisnt provided
-
-        Returns:
-            str: lastname from submitted form data
-        """
-        if self.cleaned_data["last_name"].strip() == '':
-            raise ValidationError("Last name is required.")
-        return self.cleaned_data["last_name"]
--- accounts/models.py
+++ accounts/models.py
@@ -20,6 +20,8 @@ class CustomUser(AbstractUser):
     email = models.EmailField('email address', unique=True, error_messages={
         'unique': "A user with that email already exists.",
     },)
+    first_name = models.CharField('first name', max_length=150)
+    last_name = models.CharField('last name', max_length=150)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
